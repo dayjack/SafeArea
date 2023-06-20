b30f596b21b61ca19613fa398a4be44ce4d42e2e
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-06-20 16:40:52 +0900

    ContentViewExtension, 동기실행

--- SafeArea/SafeArea/Global/Extension/ViewExtension.swift
+++ SafeArea/SafeArea/Global/Extension/ViewExtension.swift
@@ -1,53 +1,20 @@
+////
+////  ViewExtension.swift
+////  SafeArea
+////
+////  Created by ChoiYujin on 2023/06/19.
+////
 //
-//  ViewExtension.swift
-//  SafeArea
-//
-//  Created by ChoiYujin on 2023/06/19.
-//
-
 import Alamofire
 import Foundation
 import SwiftUI
-
+import CoreLocation
+//
 extension View {
-    
-    
-        func fetchZscode() {
-            
-            let url = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json"
-            let params = ["x" : "129.3252610111111", "y" : "36.0124723151111"] as Dictionary
-            let key = Bundle.main.kakaoApiKey!
-            print("key : \(key)")
-            let headers: HTTPHeaders = ["Authorization": "\(Bundle.main.kakaoApiKey!)"]
-            AF.request(url, method: .get ,parameters: params, headers: headers)
-                .responseDecodable(of: ZscodeData.self) { response in
-                    guard let data = response.value else {
-                        print("Failed to decode dangerInfoArray")
-                        return
-                    }
-                    print(response.value?.documents.first?.code ?? "4711000000")
-                    print("\(data.documents.first?.code?.prefix(4) ?? "4711")0")
-                }
-            
-        }
-    
-        func fetchChargingStation() {
-            let chargingUrl = "https://apis.data.go.kr/B552584/EvCharger/getChargerInfo?numOfRows=10&pageNo=1&zscode=47110&dataType=JSON&serviceKey=\(Bundle.main.chargingAPIKey!)"
-            
-    //        let chargingParams = ["numOfRows": "9999", "pageNo": "1", "zscode": "\(data.documents.first?.code?.prefix(4) ?? "4711")0", "dataType": "JSON", "serviceKey": "\(Bundle.main.chargingAPIKey!)"] as Dictionary
-            let chargingParams = ["numOfRows": "9999", "pageNo": "1", "zscode": "47110", "dataType": "JSON", "serviceKey": "\(Bundle.main.chargingAPIKey!)"] as Dictionary
-            
-            print("\(Bundle.main.chargingAPIKey!)")
-            
-            AF.request(chargingUrl, method: .get).responseDecodable(of: ChargingStation.self) { response in
-    //            print(self.chargingStationModelData?.items.item.first?.addr)
-                print(response.value?.items.item.first?.addr)
-            }
-        }
-    
+ 
     func fetchWeather() {
         let url = "https://api.openweathermap.org/data/2.5/weather"
-        let params = ["lat" : "36.11", "lon" : "120.99", "appid" : "\(Bundle.main.weatherAPIKey!)"] as Dictionary
+        let params = ["lat" : 36.11, "lon" : 120.99, "appid" : "\(Bundle.main.weatherAPIKey!)"] as Dictionary
         let key = Bundle.main.weatherAPIKey!
         print("key : \(key)")
         AF.request(url, method: .get ,parameters: params)
@@ -60,3 +27,4 @@ extension View {
             }
     }
 }
+//
