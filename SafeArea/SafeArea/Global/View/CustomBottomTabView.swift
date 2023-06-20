c5b48c1e81018c4632494f6b7d1a5779079d27ce
dayjack <frostjanna12@gmail.com>
2023-06-20 16:27:17 +0900

    [Design/#21] 하단 탭바 선택 색상 적용 & 커스텀 색상  넣기

--- SafeArea/SafeArea/Global/View/CustomBottomTabView.swift
+++ SafeArea/SafeArea/Global/View/CustomBottomTabView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CustomBottomTabView: View {
     
+    @State var iconSelected: IconName = .home
+    
     var body: some View {
         ZStack {
             
@@ -20,49 +22,48 @@ struct CustomBottomTabView: View {
             HStack(spacing: 57) {
                 
                 Button {
-                    //
+                    iconSelected = .guideline
                 } label: {
                     VStack(spacing: 5) {
-                        Image(IconName.guideline.rawValue)
+                        Image(iconSelected == .guideline ? "\(IconName.guideline.rawValue)_activated" : "\(IconName.guideline.rawValue)_unactivated")
                             .resizable()
                             .scaledToFit()
                             .frame(width: 31, height: 31)
                         Text("가이드라인")
                             .font(.system(size: 12))
                             .fontWeight(.medium)
+                            .foregroundColor(iconSelected == .guideline ? .safeGreen : .safeGray)
                     }
                     .foregroundColor(.black)
                 }
-                Button {
-                    
-                } label: {
-                    Image(IconName.checklist.rawValue)
-                        .frame(width: 73, height: 73)
-                        .background(.white)
-                        .clipShape(Circle())
-                        .offset(x: 0 , y: -41)
-                        .shadow(radius: 7)
-                        .foregroundColor(.black)
-                }
+            
+                Image("\(IconName.checklist.rawValue)_unactivated")
+                    .frame(width: 73, height: 73)
+                    .background(.white)
+                    .clipShape(Circle())
+                    .offset(x: 0 , y: -41)
+                    .shadow(radius: 7)
+                    .foregroundColor(.black)
+                    .onTapGesture {
+                        iconSelected = .home
+                    }
                 
                 Button {
-                    
+                    iconSelected = .checklist
                 } label: {
                     VStack(spacing: 5) {
-                        Image(IconName.checklist.rawValue)
+                        Image(iconSelected == .checklist ? "\(IconName.checklist.rawValue)_activated" : "\(IconName.checklist.rawValue)_unactivated")
                             .resizable()
                             .scaledToFit()
                             .frame(width: 31, height: 31)
                         Text("체크리스트")
                             .font(.system(size: 12))
                             .fontWeight(.medium)
+                            .foregroundColor(iconSelected == .checklist ? .safeGreen : .safeGray)
                     }
                     .foregroundColor(.black)
                 }
-
-                
             }
-            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.blue.opacity(0.4))
@@ -86,5 +87,6 @@ extension CustomBottomTabView {
     enum IconName: String {
         case guideline = "icon_guideline"
         case checklist = "icon_checklist"
+        case home = "icon_home"
     }
 }
