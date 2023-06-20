4056743fb82f50ed5559d6945d7618a861be77c1
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-06-20 21:52:03 +0900

    [Design] - GuideLineView 생성

---
+++ SafeArea/SafeArea/Global/View/GuideSelectButtonView.swift
@@ -0,0 +1,68 @@
+//
+//  GuideSelectButtonView.swift
+//  SafeArea
+//
+//  Created by 신상용 on 2023/06/20.
+//
+
+import SwiftUI
+
+struct GuideSelectButtonView: View {
+    
+    @Binding var guidePageNumber: Int
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                VStack(spacing: 17){
+                    Button(action: {
+                        guidePageNumber = 0
+                    }) {
+                        Text("전기차 관리")
+                            .fontWeight(.semibold)
+                            .foregroundColor(guidePageNumber == 0 ? Color.safeGreen : Color(hex: "C5C5C5"))
+                    }
+                    Divider()
+                        .frame(minHeight: 2)
+                        .overlay(guidePageNumber == 0 ? Color.safeGreen : Color(hex: "C5C5C5"))
+                }
+                VStack(spacing: 17){
+                    Button(action: {
+                        guidePageNumber = 1
+                    }) {
+                        Text("전기차 충전")
+                            .fontWeight(.semibold)
+                            .foregroundColor(guidePageNumber == 1 ? Color.safeGreen : Color(hex: "C5C5C5"))
+                    }
+                    Divider()
+                        .frame(minHeight: 2)
+                        .overlay(guidePageNumber == 1 ? Color.safeGreen : Color(hex: "C5C5C5"))
+                        
+                }
+                VStack(spacing: 17){
+                    Button(action: {
+                        guidePageNumber = 2
+                    }) {
+                        Text("긴급  상황")
+                            .fontWeight(.semibold)
+                            .foregroundColor(guidePageNumber == 2 ? Color.safeGreen : Color(hex: "C5C5C5"))
+                    }
+                    Divider()
+                        .frame(minHeight: 2)
+                        .overlay(guidePageNumber == 2 ? Color.safeGreen : Color(hex: "C5C5C5"))
+                }
+            }
+            .padding(.leading, 25)
+            .padding(.trailing, 25)
+            
+            .border(Color.black, width: 0)
+            
+        }
+    }
+}
+
+struct GuideSelectButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuideSelectButtonView(guidePageNumber: .constant(0))
+    }
+}
