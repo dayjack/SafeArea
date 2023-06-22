51b7840121ca05371598a27aa7e881e003e7e739
dayjack <frostjanna12@gmail.com>
2023-06-22 21:16:10 +0900

    [Feat/#78] swiftui-bottom-sheet-drawer 레이아웃만 만들기

--- SafeArea/SafeArea/App/MainMapView.swift
+++ SafeArea/SafeArea/App/MainMapView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import CoreLocation
 import MapKit
+import swiftui_bottom_sheet_drawer
 
 struct MainMapView: View {
     
@@ -17,6 +18,12 @@ struct MainMapView: View {
     @Binding var zscodeData: ZscodeData?
     @Binding var weatherData: Weather?
     @Binding var chargingStationAnnotation: [ChargingStationAnnotation]
+    
+    // MARK: - swiftui_bottom_sheet_drawer
+    @State var isFixed = false
+    @State var position: BottomSheetPosition = .down(0)
+    @State var listOpacity = 0
+    
     var coordinate: CLLocationCoordinate2D? {
         locationViewModel.lastSeenLocation?.coordinate
     }
@@ -34,21 +41,78 @@ struct MainMapView: View {
             .gesture(DragGesture().onChanged { _ in
                 userTrackingMode = .none
             })
+                BottomSheet(
+                    content:
+                            ZStack {
+                                Color.white
+                                    .cornerRadius(20)
+                                    .offset(y: -14)
+                                VStack {
+                                    
+                                    List {
+                                        
+                                        ForEach(0..<5) { _ in
+                                            VStack(alignment: .leading, spacing: 0) {
+                                                Text("충전소")
+                                                    .pretendarText(fontSize: 16, fontWeight: .medium)
+                                                    .padding(.bottom, 8)
+                                                Text("라아ㅓㅗㄹ어ㅏㅗㅁ라ㅓㅗㅇ러ㅗ아로아ㅓ로아ㅓㅗㄹ")
+                                                    .pretendarText(fontSize: 16, fontWeight: .medium)
+                                                    .padding(.bottom, 7)
+                                                Text("포항시 남구 지곡동")
+                                                    .pretendarText(fontSize: 12, fontWeight: .medium)
+                                                    .foregroundColor(Color.init(hex: "808080"))
+                                            }
+                                        }
+                                    }
+                                    .listStyle(.plain)
+                                    .opacity(Double(listOpacity))
+                                    
+                                }
+                                .padding(.leading, 4)
+                                .padding(.vertical, 26)
+                                .opacity(isFixed ? 0 : 1)
+                            }
+                        .shadow(color: .black.opacity(0), radius: 0, x: 0, y: 0)
+                    ,
+                    shift: 154,
+                    topIndentation: 184
+                )
+            .onPositionChanged{
+                position = $0
+                if !position.isDown {
+                    withAnimation {
+                        listOpacity = 1
+                    }
+                } else {
+                    listOpacity = 0
+                }
+            }
+            .shadow(color: .black.opacity(0.15), radius: 3.5, x: 0, y: -1)
+            .opacity(isFixed ? 0 : 1)
             
+            
+            VStack {
+                Spacer()
+                Color.white.frame(height: 154)
+                
+            }
+            .ignoresSafeArea()
+            .opacity(isFixed ? 1 : 0)
             
             VStack(spacing: 0) {
                 
                 ZStack {
                     MainMapTopView(locationViewModel: $locationViewModel, zscodeData: $zscodeData, weatherData: $weatherData)
                     //매개변수 locationViewModel: $locationViewModel, zscodeData: $zscodeData, chargingStationModelData: $chargingStationModelData, weatherData: $weatherData
-//                    Image("Union2")
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fit)
-//
-////                    print("날씨 데이터 \(self.weatherData!.weather?.first?.description ?? "날씨 안되")")
-////                    print("날씨 온도 \(self.weatherData?.main?.temp)")
-//
-//                    Text("날씨 데이터 \(self.weatherData?.weather?.first?.id ?? 0)")
+                    //                    Image("Union2")
+                    //                        .resizable()
+                    //                        .aspectRatio(contentMode: .fit)
+                    //
+                    ////                    print("날씨 데이터 \(self.weatherData!.weather?.first?.description ?? "날씨 안되")")
+                    ////                    print("날씨 온도 \(self.weatherData?.main?.temp)")
+                    //
+                    //                    Text("날씨 데이터 \(self.weatherData?.weather?.first?.id ?? 0)")
                     
                 }
                 
@@ -71,15 +135,17 @@ struct MainMapView: View {
                                     .scaledToFit()
                                     .frame(width: 18.4, height: 18.4)
                                     .foregroundColor(.black)
-                                    
+                                
                             }
                             .shadow(radius: 4, x: 0 , y: 1)
                     }
+                    .opacity(position.isDown ? 1 : 0)
+                    
                 }
                 .padding(.horizontal, 26)
-                .padding(.bottom, 116)
+                .padding(.bottom, 180)
                 
-
+                
                 
             }
             
