f59295a30e588757290a4471b249cc1bed2c0bd7
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-06-27 23:51:39 +0900

    [Refactor] - 중복코드 제거

--- SafeArea/SafeArea/App/MapView.swift
+++ SafeArea/SafeArea/App/MapView.swift
@@ -28,36 +28,6 @@ struct MapView: View {
         .gesture(DragGesture().onChanged { _ in
             userTrackingMode = .none
         })
-        .onAppear {
-            
-            let url = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json"
-            let params = ["x" : "129.3252610111111", "y" : "36.0124723151111"] as Dictionary
-            let key = Bundle.main.kakaoApiKey!
-            print("key : \(key)")
-            let headers: HTTPHeaders = ["Authorization": "\(Bundle.main.kakaoApiKey!)"]
-            AF.request(url, method: .get ,parameters: params, headers: headers)
-                .responseDecodable(of: ZscodeData.self) { response in
-                    guard let data = response.value else {
-                        print("Failed to decode dangerInfoArray")
-                        return
-                    }
-                    self.zscodeData = data
-                    print(response.value?.documents.first?.code ?? "4711000000")
-                    print("\(data.documents.first?.code?.prefix(4) ?? "4711")0")
-                    
-                    let chargingUrl = "https://apis.data.go.kr/B552584/EvCharger/getChargerInfo?numOfRows=100&pageNo=1&zscode=47110&dataType=JSON&serviceKey=\(Bundle.main.chargingAPIKey!)"
-                    
-                    let chargingParams = ["numOfRows": "9999", "pageNo": "1", "zscode": "\(data.documents.first?.code?.prefix(4) ?? "4711")0", "dataType": "JSON", "serviceKey": "\(Bundle.main.chargingAPIKey!)"] as Dictionary
-                    
-                    print("\(Bundle.main.chargingAPIKey!)")
-                    
-                    AF.request(chargingUrl, method: .get).responseDecodable(of: ChargingStation.self) { response in
-                        self.chargingStationModelData = response.value
-                        print(self.chargingStationModelData?.items.item.first?.addr)
-                    }
-                }
-            fetchWeather()
-        }
     }
 }
 
