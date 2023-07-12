64351aa9d89cddb76b7419ddf796e1a31d59faff
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-07-12 23:19:22 +0900

    [Design] - Onboarding Resizing

--- SafeArea/SafeArea/App/OnboardingView.swift
+++ SafeArea/SafeArea/App/OnboardingView.swift
@@ -15,29 +15,39 @@ struct OnboardingView: View {
         ZStack {
             TabView(selection: $pageNm) { // selection 매개변수로 현재 페이지 번호 추적
                 Image("onbo_1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
                     .ignoresSafeArea()
                     .tag(1) // 페이지 번호 태그
                 
                 Image("onbo_2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
                     .ignoresSafeArea()
                     .tag(2)
                 
                 Image("onbo_3")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
                     .ignoresSafeArea()
                     .tag(3)
                 
                 Image("onbo_4")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
                     .ignoresSafeArea()
                     .tag(4)
                 
                 ZStack {
                     Image("onbo_5")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
                         .ignoresSafeArea()
                     VStack {
                         Spacer()
                         OnboardingButton()
                             .frame(width: 217, height: 52)
-                            .padding(.bottom, 70)
+                            .padding(.bottom, onboardingButtonSize())
                             .onTapGesture {
                                  isOnboardingViewActive = false
                                 print("dd")
@@ -51,7 +61,7 @@ struct OnboardingView: View {
             
             VStack(spacing: 0) {
                 OnboardingIndicator(pageNm: $pageNm)
-                    .padding(.top, 90)
+                    .padding(.top, onboardingIndicatorSize())
                     .padding(.bottom, 23)
                 Spacer()
             }
